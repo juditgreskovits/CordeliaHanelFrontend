5ce0ae0e63f71e53600e9e0d634ce797b851ffc0
Judit Greskovits <juditgreskovits@gmail.com>
2014-04-27 20:20:47 +0100

    adding fix

--- www/app/scripts/case_study.js
+++ www/app/scripts/case_study.js
@@ -7,7 +7,7 @@ studioHanel.CaseStudy = function() {
 		$.each(bulletsData, function(index, stage) {
 			var selectorIndex = index + 1;
 			var selector = '#case-study .container-fluid .row #cell' + selectorIndex;
-			$(selector).append('<img src="' + stage.image + '">');
+			$(selector).append('<img src="../' + stage.image + '">');
 			var caption = stage.caption;
 			caption = '<h3><span style="color:#806854">' + caption.substring(0,1) + 
 				'</span><span style="color:#a8baa9">' + caption.substring(1,2) + '</span>' + caption.substring(2) + '</h3>'
