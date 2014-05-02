a66171ed3af12eea9fb042a2efaf1a45b68b2503
Judit Greskovits <juditgreskovits@gmail.com>
2014-05-02 10:34:06 +0100

    image fix

--- www/app/scripts/interiors.js
+++ www/app/scripts/interiors.js
@@ -318,7 +318,7 @@ studioHanel.Gallery = function(interiorIndex) {
 		//if(image.left < windowWidth) {
 			var imageClass = imageIndex == galleryIndex ? 'active' : 'inactive';
 			if(!htmlExists) {
-				$('#images').append('<img id="' + imageId + '" src="' + image.src + '">');
+				$('#images').append('<img id="' + imageId + '" src="../' + image.src + '">');
 				var targetAlpha = imageIndex == galleryIndex ? 1.0 : 0.33;
 				TweenMax.to($(selector), 1.2, {autoAlpha:targetAlpha, ease:Sine.easeOut});
 			}
